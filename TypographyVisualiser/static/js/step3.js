e1e187b7d94d962c57df51a8ab9874aaf157de51
Niklas Tluk <niklas.tluk@study.hs-duesseldorf.de>
2020-07-14 16:24:14 +0200

    refined outline mode

--- TypographyVisualiser/static/js/step3.js
+++ TypographyVisualiser/static/js/step3.js
@@ -1,7 +1,37 @@
+$.ajax({
+            url: "/Outline",
+            method: 'POST'
+        })
+    .done(data => {
+                // read imagepaths from data into list
+                let stringRep = data
+                let parts = stringRep.split("\"");
+
+                let filepaths = [];
+                for(let i = 0; i < parts.length; i++){
+                    if(i % 2 != 0)
+                        filepaths.push(parts[i]);
+                }
+
+            // Ladeanimation abspielen
+            $("#image").empty();
+
+                setTimeout(() => {
+                for (let path of filepaths){
+                    let imageSource = `${path}?${(new Date()).getTime()}`;
+                    let newImage = $("<img />").attr({"src": imageSource, "class": "img-result"});
+
+                    $("#image").append(newImage);
+                }
+            }, 2000)
+
+        })
+    }
+
 $("#redirect-step-1").on("click", event => {
     event.preventDefault();
     window.location.href = "/Start"
-
+ }
     $.ajax({
         url: "/resetFolders",
         method: 'POST'
