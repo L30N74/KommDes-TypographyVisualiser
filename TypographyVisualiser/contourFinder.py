31ef3fedc3e36c57c9ba3e8c886079df542f4ea7
Angela <playerqueen@gmx.de>
2020-07-16 12:42:14 +0200

    improve draw outline, move script from step3.html in step3.js, rename variables

--- TypographyVisualiser/contourFinder.py
+++ TypographyVisualiser/contourFinder.py
@@ -3,57 +3,59 @@ import cv2
 import numpy as np
 
 from PIL import Image, ImageFont, ImageDraw
+import scipy.misc
 from flask import Blueprint, request, json
-
+import matplotlib
 
 contourFinder = Blueprint("contourFinder", __name__, static_folder="static")
 
 
-@contourFinder.route("/Outline", methods=['POST'])
+@contourFinder.route("/Outline", methods=['GET'])
 def draw_outline():
-
     image_path = "static/output/result-0.jpg"
-    font_type = ImageFont.truetype('C:\Windows\Fonts\Arial.ttf', 15)
-    searchterm = "duck"
+    background_image_path = "static/output/final-result-0.jpg"
+    font_type = ImageFont.truetype('fonts/Roboto-Light.ttf', 20)
+    search_term = "H"
     color = (255, 69, 0)
 
-    image = read_image(image_path)
-    # image.show()
+    image_template = Image.open(image_path)
+    image_template = resize_image(image_template)
+    # preparation for final-result background 
+    create_background_image()
+    background_image = Image.open(background_image_path)
+    #
+    image_template_array = np.array(image_template)
 
-    img_array = np.array(image)
-
-    # np.set_printoptions(threshold=np.inf)
-    print(img_array)
+    # draw on background image
+    draw = ImageDraw.Draw(background_image)
     y = 0
     x = 0
     i = 0
-    draw = ImageDraw.Draw(image)
 
-    for line in img_array:
+    for line in image_template_array:
 
-        for p in line:
+        for pixel in line:
 
-            if p >= 250:
+            if pixel >= 250:
                 # print(p)
                 # draw = ImageDraw.Draw(image)
 
                 if i % 10 == 0:
-                    draw.text(xy=(x, y), text=searchterm, fill=230, font=font_type)
+                    draw.text(xy=(x, y), text=search_term, fill=color, font=font_type)
                 i = i + 1
-                p = 0
+                pixel = 0
 
-            else:
-                print("nope")
-            print(x)
+            # else:
+                # print("nope")
+            # print(x)
 
             x = x + 1
 
-        print(y)
+        # print(y)
         y = y + 1
         x = 0
 
-    # image.show()
-    image.save(image_path)
+    background_image.save(background_image_path)
 
     return "Nothing"
 
@@ -63,16 +65,22 @@ def fill_outline():
     pass
 
 
-def read_image(path):
+def resize_image(img):
     try:
-        # img = PIL.Image.open(path)
-
-        size = 1280, 720
-        img = Image.open(path)
+        size = 1920, 1080
         img_resized = img.resize(size, Image.ANTIALIAS)
         return img_resized
     except Exception as e:
         print(e)
 
 
-
+def create_background_image():
+    try:
+        background_image_array = np.zeros((1080, 1920))
+        # background_image = Image.fromarray(background_image_array)
+        # background_image.save("final-result-0.jpg")
+        # scipy.misc.imsave('outfile.jpg', background_image_array)
+        matplotlib.image.imsave('static/output/final-result-0.jpg', background_image_array)
+        print("background finish")
+    except Exception as e:
+        print(e)
--- TypographyVisualiser/static/js/step3.js
+++ TypographyVisualiser/static/js/step3.js
@@ -1,39 +1,19 @@
-$.ajax({
-            url: "/Outline",
-            method: 'POST'
-        })
-    .done(data => {
-                // read imagepaths from data into list
-                let stringRep = data
-                let parts = stringRep.split("\"");
-
-                let filepaths = [];
-                for(let i = 0; i < parts.length; i++){
-                    if(i % 2 != 0)
-                        filepaths.push(parts[i]);
-                }
-
-            // Ladeanimation abspielen
-            $("#image").empty();
-
-                setTimeout(() => {
-                for (let path of filepaths){
-                    let imageSource = `${path}?${(new Date()).getTime()}`;
-                    let newImage = $("<img />").attr({"src": imageSource, "class": "img-result"});
-
-                    $("#image").append(newImage);
-                }
-            }, 2000)
-
-        })
-    }
-
 $("#redirect-step-1").on("click", event => {
+    $.ajax({
+    url: "/resetFolders",
+    method: 'POST'
+})
     event.preventDefault();
     window.location.href = "/Start"
- }
-    $.ajax({
-        url: "/resetFolders",
-        method: 'POST'
-    })
 })
+$.ajax({
+    url: "/Outline",
+    method: 'GET'
+})
+    .done(data => {
+        let resultImage = $("<img>").attr({"class": "img-result", "src": "../static/output/final-result-0.jpg"});
+        resultImage.appendTo('#image')
+        $("<p>Test</p>").appendTo("#image")
+
+    })
+
