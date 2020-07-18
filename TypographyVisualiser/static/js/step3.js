6b0c387a00a150f433729d1b7044106377ebd93c
Angela <playerqueen@gmx.de>
2020-07-18 20:19:33 +0200

    on step3 add download button

--- TypographyVisualiser/static/js/step3.js
+++ TypographyVisualiser/static/js/step3.js
@@ -14,6 +14,7 @@ $.ajax({
         let resultImage = $("<img>").attr({"class": "img-result", "src": "../static/output/final-result-0.jpg"});
         resultImage.appendTo('#image')
         $(".load-spinner").attr({"style": "display: none"})
+        $("#download-button").attr({"style": "display: inline"})
 
     })
 
